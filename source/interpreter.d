4107c22ab696800a57185cee0cb0fa9a10e880d7
DarkSystemGit <85317259+DarkSystemGit@users.noreply.github.com>
2024-09-12 19:26:43 -0400

    eod, string parsing

--- source/interpreter.d
+++ source/interpreter.d
@@ -15,7 +15,7 @@ struct Registers {
     int a;
     int b;
     int c;
-    int* d;
+    int d;
     string e;
     float f;
     float g;
@@ -113,7 +113,7 @@ int cp(ref Machine machine, real[] params) {
     handleRegisters(machine, params, 1);
     for (int j = 0; j < 9; j++) {
             if (params[1] == (real.max - j)) {
-                if(j!=5)acessRegister(machine, j)=params[1];
+                if(j!=5)setRegister(machine, j,params[1]);
             }
         }
     return 0;
@@ -172,9 +172,46 @@ real acessRegister(ref Machine machine, real id) {
         return cast(real)machine.registers.j;
         break;
     }
-
+return 0;
         
 }
+void setRegister(ref Machine machine, real id, real value) {
+    switch (cast(int)id) {
+        default:
+        break;
+        case (9):
+        machine.registers.a = cast(int)value;
+        break;
+        case (8):
+        machine.registers.b = cast(int)value;
+        break;
+        case (7):
+        machine.registers.c = cast(int)value;
+        break;
+        case (6): 
+        machine.registers.d = cast(int)value;
+        break;
+        case (4):
+        machine.registers.f = cast(float)value;
+        break;
+        case (3):
+        machine.registers.g = cast(float)value;
+        break;
+        case (2):
+        machine.registers.h = cast(double)value;
+        break;
+        case (1):
+        machine.registers.i = value;
+        break;
+        case (0):
+        machine.registers.j = value;
+        break;
+
+        
+
+    }
+}
+    
 void handleFlags(ref Machine machine, real res) {
     Flags flags= machine.flags;
     if (res == 0) flags.zero = true;
@@ -198,4 +235,143 @@ Machine parse(real[] prgm) {
         writeln(machine.registers);
     }
     return machine;
+}
+real[] compile(string[] source) {
+    bool eof = false;
+    real[] prgm = new real[0];
+   for(int i = 0; i < source.length; i++) {
+        string line = source[i];
+        real res = 0;
+        if (line.indexOf("0x") != -1) {
+            res = cast(real)chompPrefix(line, "0x").to!uint(16);
+        } else if (line.isNumeric()) {
+            res =  cast(real)line.to!uint(10);
+        } else {
+            switch (line) {
+            case "%A":
+                res = real.max - 9;
+                break;
+            case "%B":
+                res = real.max - 8;
+                break;
+            case "%C":
+                res = real.max - 7;
+                break;
+            case "%D":
+                res = real.max - 6;
+                break;
+            case "%E":
+                res = real.max - 5;
+                break;
+            case "%F":
+                res = real.max - 4;
+                break;
+            case "%G":
+                res = real.max - 3;
+                break;
+            case "%H":
+                res = real.max - 2;
+                break;
+            case "%I":
+                res = real.max - 1;
+                break;
+            case "%J":
+                res = real.max;
+                break;
+            case "add":
+                res = 1;
+                break;
+            case "sub":
+                res = 2;
+                break;
+            case "mul":
+                res = 3;
+                break;
+            case "addf":
+                res = 4;
+                break;
+            case "subf":
+                res = 5;
+                break;
+            case "mulf":
+                res = 6;
+                break;
+            case "and":
+                res = 7;
+                break;
+            case "not":
+                res = 8;
+                break;
+            case "or":
+                res = 9;
+                break;
+            case "xor":
+                res = 10;
+                break;
+            case "cp":
+                res = 11;
+                break;
+            case "jmp":
+                res = 12;
+                break;
+            case "jnz":
+                res = 13;
+                break;
+            case "jz":
+                res = 14;
+                break;
+            case "cmp":
+                res = 15;
+                break;
+            case "sys":
+                res = 16;
+                break;
+            case "read":
+                res = 17;
+                break;
+            case "write":
+                res = 18;
+                break;
+            case "push":
+                res = 19;
+                break;
+            case "pop":
+                res = 20;
+                break;
+            case "mov":
+                res = 21;
+                break;
+            case "call":
+                res = 22;
+                break;
+            case "ret":
+                res = 23;
+                break;
+            case "inc":
+                res = 24;
+                break;
+            case "dec":
+                res = 25;
+                break;
+            case "incf":
+                res = 26;
+                break;
+            case "decf":
+                res = 27;
+                break;
+            
+            default:
+                break;
+            }
+        }
+
+        
+
+        prgm.length++;prgm[prgm.length - 1] = res;
+
+    }
+    return prgm;
+}
+Machine parseString(string source) {
+    return parse(compile(source.replace(','," ").replace(";"," ").split(" ")));
 }
