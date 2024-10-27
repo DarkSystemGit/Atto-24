98477a8933521a3cd09f737a894e057872a1ea30
DarkSystemGit <85317259+DarkSystemGit@users.noreply.github.com>
2024-10-26 21:12:44 -0400

    tokenizer

--- source/parser.d
+++ source/parser.d
@@ -1,3 +1,4 @@
+import std;
 enum TokenType{
     REG_A,
     REG_B,
@@ -45,21 +46,123 @@ enum TokenType{
     DEFINE,
     NUMBER,
     NULL,
-    IDENTIFIER
+    IDENTIFIER,
+    EOF,
+    SEMICOLON
 }
 struct Token
 {
-    TokenType token;
+    TokenType type;
     string literal;
+}
+class Tokenizer{
+    string source;
+    int pos;
+    int start;
+    Token[] tokens;
+    Token scanToken(string src){
+        string c=advance();
+        switch(c){
+            case "\"":
+                string s;
+                while(peek()!="\""){
+                    s~=advance();
+                }   
+                advance();
+                addToken(TokenType.STRING,s); 
+                return;
+            case "/":
+                string s; 
+                advance();
+                while(peek()!=";"){
+                    s~=advance();
+                }
+                return;
+            default:
+                if(std.ascii.isDigit(c)){
+                    string n=c;
+                    while(std.ascii.isDigit(peek())||(peek()=="."&&isDigit(peekNext()))){
+                        n~=advance();
+                    }
+                    addToken(TokenType.NUMBER,n);
+                }
+                return;
+        }
+    }
+    bool isDigit(string c){
+    void scanTokens(string src){
+        this.source=src;
+        this.pos=0;
+        while(!isAtEnd()){
+            start=pos;
+            scanToken(this.source);
+        }
+        TokenType eof=TokenType.EOF;
+        addToken(eof);
+    }
+    void addToken(Token t){
+        tokens.length++;
+        tokens[tokens.length-1]=t;
+    }   
+    void addToken(TokenType t){
+        Token t;
+        t.type=t;
+        addToken(t);
+    }
+    void addToken(TokenType t,string l){
+        Token t;
+        t.type=t;
+        t.literal=l;
+        addToken(t);
+    }
+    bool isAtEnd(){
+        return pos>=source.length;
+    }
+    string advance(){
+        return source[pos++];
+    }    
+    string peek(){
+        if(isAtEnd())return "";
+        return source[pos];
+    }    
+    string peekNext(){
+        if(pos+1>=source.length)return "";
+        return source[pos+1];
+    }    
+    bool match(string c){
+        if(isAtEnd())return false;
+        if(source[pos]!=c)return false;
+        pos++;
+        return true;
+    }
 }
 class Parser{
     Token[] tokens;
     int pos;
     bool err;
     void consume(TokenType t,string err){
-        if(check(t))return advance;
+        if(check(t))return advance();
         throw new Exception(err);
     }
-    
-    voidd error(string msg);
+    bool check(TokenType t){
+        if(isAtEnd())return false;
+        return peek().type==t;
+    }
+    Token advance(){
+        if(!isAtEnd())pos++;
+        return previous();
+    }
+    bool isAtEnd(){
+        return peek().type==TokenType.EOF;
+    }
+    Token peek(){
+        return tokens[pos];
+    }
+    Token previous(){
+        return tokens[pos-1];
+    } 
+    void error(string msg){
+        err=true;
+        throw new Exception("Error at Token ",pos,":",msg);
+    };
 }
