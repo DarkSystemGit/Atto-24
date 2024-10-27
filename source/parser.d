b02f9e878ecd55e5587fab2e09a768a7baed06b6
DarkSystemGit <85317259+DarkSystemGit@users.noreply.github.com>
2024-10-27 01:27:47 +0000

    more token stuff

--- source/parser.d
+++ source/parser.d
@@ -1,5 +1,7 @@
 import std;
-enum TokenType{
+
+enum TokenType
+{
     REG_A,
     REG_B,
     REG_C,
@@ -50,119 +52,174 @@ enum TokenType{
     EOF,
     SEMICOLON
 }
+
 struct Token
 {
     TokenType type;
     string literal;
 }
-class Tokenizer{
+
+class Tokenizer
+{
     string source;
     int pos;
     int start;
     Token[] tokens;
-    Token scanToken(string src){
-        string c=advance();
-        switch(c){
-            case "\"":
-                string s;
-                while(peek()!="\""){
-                    s~=advance();
-                }   
-                advance();
-                addToken(TokenType.STRING,s); 
-                return;
-            case "/":
-                string s; 
-                advance();
-                while(peek()!=";"){
-                    s~=advance();
+    void scanToken(string src)
+    {
+        char c = advance();
+        switch (c)
+        {
+        case '"':
+            string s;
+            while (peek() != '"')
+            {
+                s ~= advance();
+            }
+            advance();
+            addToken(TokenType.STRING, s);
+            return;
+        case '/':
+            string s;
+            advance();
+            while (peek() != ';')
+            {
+                s ~= advance();
+            }
+            return;
+        default:
+            if (std.ascii.isDigit(cast(char)c))
+            {
+                string n;
+                n~=c;
+                while (std.ascii.isDigit(cast(char)peek()) || (peek() == '.' && isDigit(cast(char)peekNext())))
+                {
+                    n ~= advance();
                 }
-                return;
-            default:
-                if(std.ascii.isDigit(c)){
-                    string n=c;
-                    while(std.ascii.isDigit(peek())||(peek()=="."&&isDigit(peekNext()))){
-                        n~=advance();
-                    }
-                    addToken(TokenType.NUMBER,n);
-                }
-                return;
+                addToken(TokenType.NUMBER, n);
+            }
+            return;
         }
     }
-    bool isDigit(string c){
-    void scanTokens(string src){
-        this.source=src;
-        this.pos=0;
-        while(!isAtEnd()){
-            start=pos;
-            scanToken(this.source);
+
+    
+        void scanTokens(string src)
+        {
+            this.source = src;
+            this.pos = 0;
+            while (!isAtEnd())
+            {
+                start = pos;
+                scanToken(this.source);
+            }
+            TokenType eof = TokenType.EOF;
+            addToken(eof);
         }
-        TokenType eof=TokenType.EOF;
-        addToken(eof);
+
+        void addToken(Token t)
+        {
+            tokens.length++;
+            tokens[tokens.length - 1] = t;
+        }
+
+        void addToken(TokenType type)
+        {
+            Token t;
+            t.type = type;
+            addToken(t);
+        }
+
+        void addToken(TokenType type, string l)
+        {
+            Token t;
+            t.type = type;
+            t.literal = l;
+            addToken(t);
+        }
+
+        bool isAtEnd()
+        {
+            return pos >= source.length;
+        }
+
+        char advance()
+        {
+            return source[pos++];
+        }
+
+        char peek()
+        {
+            if (isAtEnd())
+                return cast(char)0;
+            return source[pos];
+        }
+
+        char peekNext()
+        {
+            if (pos + 1 >= source.length)
+                return cast(char)0;
+            return source[pos + 1];
+        }
+
+        bool match(char c)
+        {
+            if (isAtEnd())
+                return false;
+            if (source[pos] != c)
+                return false;
+            pos++;
+            return true;
+        }
     }
-    void addToken(Token t){
-        tokens.length++;
-        tokens[tokens.length-1]=t;
-    }   
-    void addToken(TokenType t){
-        Token t;
-        t.type=t;
-        addToken(t);
+
+    class Parser
+    {
+        Token[] tokens;
+        int pos;
+        bool err;
+        Token consume(TokenType t, string err)
+        {
+            if (check(t))
+                return advance();
+            throw new Exception(err);
+        }
+
+        bool check(TokenType t)
+        {
+            if (isAtEnd())
+                return false;
+            return peek().type == t;
+        }
+
+        Token advance()
+        {
+            if (!isAtEnd())
+                pos++;
+            return previous();
+        }
+
+        bool isAtEnd()
+        {
+            return peek().type == TokenType.EOF;
+        }
+
+        Token peek()
+        {
+            return tokens[pos];
+        }
+
+        Token previous()
+        {
+            return tokens[pos - 1];
+        }
+
+        void error(string msg)
+        {
+            err = true;
+            throw new Exception("Error at Token "~ pos.to!string~":"~msg);
+        };
     }
-    void addToken(TokenType t,string l){
-        Token t;
-        t.type=t;
-        t.literal=l;
-        addToken(t);
-    }
-    bool isAtEnd(){
-        return pos>=source.length;
-    }
-    string advance(){
-        return source[pos++];
-    }    
-    string peek(){
-        if(isAtEnd())return "";
-        return source[pos];
-    }    
-    string peekNext(){
-        if(pos+1>=source.length)return "";
-        return source[pos+1];
-    }    
-    bool match(string c){
-        if(isAtEnd())return false;
-        if(source[pos]!=c)return false;
-        pos++;
-        return true;
-    }
-}
-class Parser{
-    Token[] tokens;
-    int pos;
-    bool err;
-    void consume(TokenType t,string err){
-        if(check(t))return advance();
-        throw new Exception(err);
-    }
-    bool check(TokenType t){
-        if(isAtEnd())return false;
-        return peek().type==t;
-    }
-    Token advance(){
-        if(!isAtEnd())pos++;
-        return previous();
-    }
-    bool isAtEnd(){
-        return peek().type==TokenType.EOF;
-    }
-    Token peek(){
-        return tokens[pos];
-    }
-    Token previous(){
-        return tokens[pos-1];
-    } 
-    void error(string msg){
-        err=true;
-        throw new Exception("Error at Token ",pos,":",msg);
-    };
+void main(){
+    Tokenizer tk=new Tokenizer();
+    
 }
